12026239cd7b81936547b7f6c710aaab51a958ed
Vagner Batista <vagnereletronica@gmail.com>
2021-12-23 17:02:53 -0300

    MUI funcionando

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { Typography, Container } from '@mui/material';
+import Link from '../components/CustomLink';
 
 export default function HomePage() {
   return (
     <Container maxWidth="md">
       <Typography element="h1" variant="h1">
-        Home Page
+        <Link href="/" underline="none">
+          {' '}
+          Home Page
+        </Link>
       </Typography>
     </Container>
   );
